a0dad2cc2a06bf30e076d4604c63c26903840333
EAMoiseenko <s1lverhron@gmail.com>
2022-08-19 16:28:39 +0300

    Added codeception setup

---
+++ tests/_bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/** @noinspection PhpDefineCanBeReplacedWithConstInspection */
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . '/../vendor/autoload.php';
+ob_start();
